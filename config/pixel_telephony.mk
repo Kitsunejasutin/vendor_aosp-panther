e52d32cb13c54b98aa8800ed321520fd67eea08e
Dyneteve <dyneteve@hentaios.com>
2022-11-17 18:26:21 +0100

    CarrierConfigs: Import carrier-specific MCC configuration
    The configuration is fully imported from latest QPR build
    
    Test: Boot, confirm overlays applied.
    
    Change-Id: I3212ce37546af2234c7c15f368f7a41e7ad47170

--- config/pixel_telephony.mk
+++ config/pixel_telephony.mk
@@ -192,3 +192,7 @@ PRODUCT_PACKAGES += \
 # Disable mobile data in roaming by default.
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.com.android.dataroaming=false
+
+# Include Carrier Runtime Configuration
+PRODUCT_PACKAGE_OVERLAYS += \
+    vendor/aosp/CarrierSettings/overlay
